2a07a39d4db3ab13a750b7ec5e28b74266d5417e
Suman <sumamyon.am@outlook.com>
2016-08-20 23:22:18 +0545

    Worked in RegisterRequest.java Worked in RegisterActivity.java and AndroidMenifest.xml

--- app/src/main/java/com/example/suman/setup/RegisterActivity.java
+++ app/src/main/java/com/example/suman/setup/RegisterActivity.java
@@ -1,14 +1,13 @@
 package com.example.suman.setup;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
@@ -30,12 +29,13 @@ public class RegisterActivity extends AppCompatActivity {
 
         final Button bRegister = (Button)findViewById(R.id.bRegister);
 
+
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String name = edName.getText().toString();
                 final String username = edUsername.getText().toString();
-                final String passeord = edPassword.getText().toString();
+                final String password = edPassword.getText().toString();
                 final int age = Integer.parseInt(edAge.getText().toString());
 
                 Response.Listener<String> responseListener = new Response.Listener<String>(){
@@ -50,10 +50,10 @@ public class RegisterActivity extends AppCompatActivity {
                                 RegisterActivity.this.startActivity(intent);
                             }else {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("Register Failed");
-                                builder.setNegativeButton("Retry",null);
-                                builder.create();
-                                builder.show();
+                                builder.setMessage("Register Failed")
+                                       .setNegativeButton("Retry",null)
+                                       .create()
+                                       .show();
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -61,7 +61,7 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 };
 
-                RegisterRequest registerRequest = new RegisterRequest(name, username,age,passeord,responseListener);
+                RegisterRequest registerRequest = new RegisterRequest(name, username,age,password,responseListener);
                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                 queue.add(registerRequest);
             }
