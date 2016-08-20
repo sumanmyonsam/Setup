d00601844f6d425d9a1b432c0e5ed5af04abbd10
sumanmyonsam <sumanmyon.sam@outlook.com>
2016-08-20 23:35:55 +0545

    updated Register.php
    :)

--- Register.php
+++ Register.php
@@ -1,13 +1,13 @@
 <?php
-	$con = mysqli_connect("my_host","my_user","my_password","my_db");
+	$con = mysqli_connect("mysql8.000webhost.com","a5698947_user","User123","a5698947_setup");
 	
 	$name = $_POST["name"];
 	$age = $_POST["age"];
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 	
-	$statement = mysqli_prepare($con,"INSERT INTO (name,age,username,password) VALUES(?,?,?,?,)");
-	mysqli_stmt_bind_param($statement,"siss",$username,$password);
+	$statement = mysqli_prepare($con,"INSERT INTO user (name,age,username,password) VALUES(?, ?, ?, ?)");
+	mysqli_stmt_bind_param($statement, "siss",$name,$age,$username,$password);
 	mysqli_stmt_execute($statement);
 	
 	$response = array();
